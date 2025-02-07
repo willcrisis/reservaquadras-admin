9b766931ebe0e7c7e7cfcd4d48e5ec00e4f8c9ae
Willian Krause <krause.willian@gmail.com>
2025-02-07 16:40:52 -0300

    Add functions support

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from 'firebase-functions/v2/https';
+import { onDocumentCreated } from 'firebase-functions/v2/firestore';
+import * as logger from 'firebase-functions/logger';
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+initializeApp();
+
+export const addMessage = onRequest(async (request, response) => {
+  const { text } = request.query;
+  const snapshot = await getFirestore().collection('messages').add({ text });
+  response.json({ result: `Message with ID: ${snapshot.id} added` });
+});
+
+export const makeUppercase = onDocumentCreated('/messages/{documentId}', async (event) => {
+  const text = event.data?.data().text;
+  logger.log('Uppercasing this shit', event.params.documentId, text);
+  const uppercase = text.toUpperCase();
+  return event.data?.ref.set({ uppercase: uppercase }, { merge: true });
+});
